2abd024f62e4a7870fa8c097d78c6fef20d4f870
Deeppanchal2108 <deepanchal2290@gmail.com>
2025-08-28 21:04:53 +0000

    feat: implement request handling with dynamic responses

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,21 @@ use hyper::service::{make_service_fn, service_fn};
 use std::convert::Infallible;
 
 
-async fn handle(_req:Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new(Body::from("Hello, World!")))
+async fn handle(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let path = req.uri().path();
+    match path {
+        "/" => Ok(Response::new(Body::from("Hello, World!"))),
+        "/api/name"=>Ok(Response::new(Body::from("My name is Deep Panchal"))),
+         "/api/age"=>Ok(Response::new(Body::from("My age is 20"))),  
+        _ => Ok(Response::new(Body::from("Not Found"))),
+    }
 }
 
 
 
 #[tokio::main]
-async fn main(){
-
+async fn main() {
+    // Address to bind to
     let addr = ([127, 0, 0, 1], 3000).into();
 
     let make_svc = make_service_fn(|_conn| async {
